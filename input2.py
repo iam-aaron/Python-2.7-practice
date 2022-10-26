eb6dad073fadfb9693420331cd60f2d8170317fc
Aaron M <aaronmedina30@gmail.com>
2022-10-26 21:43:27 +0800

    playing with user inputs

---
+++ input2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+height = float(raw_input("What is your height? (inches or meters) "))
+weight = float(raw_input("What is your weight? (pounds  or kilograms) "))
+unit = raw_input
