bda0ae3dc1f61f885bcdea1ccdc5444e4a8ff9e2
Aaron M <aaronmedina30@gmail.com>
2022-11-15 00:03:05 +0800

    added pgdump module

---
+++ pgbackup/tests/test_pgdump.py
@@ -0,0 +1,24 @@
+import pytest
+import subprocess
+
+from pgbackup import pgdump
+
+url = "postgres://demo-user:password@18.212.176.143:80/sample"
+
+def test_dump_call_pg_dump(mocker): #fixture from mock package
+    """
+    Utilize pg_dump to interact with Database.
+    """
+    proc = mocker.Mock()
+    mocker.patch('subprocess.Popen', return_value=proc)
+    assert pgdump.dump(url) == proc
+    subprocess.Popen.assert_called_with(['pg_dump', url], stdout=subprocess.PIPE)
+
+def test_dump_handles_oserror(mocker):
+    """
+    pgdump.dump returns a reasonable error if pg_dump isn't installed.
+    """
+    mocker.patch('subprocess.Popen', side_effect=OSError("no such file"))
+    with pytest.raises(SystemExit):
+        pgdump.dump(url)
+
